7f1cb106fe72bf5dd3b482a08bb8ff2eafcc8532
Tre Hultzen <hultzentre@gmail.com>
2024-04-07 20:00:10 -0700

    Added leafnodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -15,6 +15,15 @@ class TestHTMLNode(unittest.TestCase):
             ' class="greeting" href="https://boot.dev"',
         )
 
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
 
 if __name__ == "__main__":
     unittest.main()
+
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,13 @@
-class TextNode():
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
 
-    def __init__(self, text, text_type, url = None):
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -12,6 +19,5 @@ class TextNode():
             and self.url == other.url
         )
 
-
-    def repr(self):
-        return f'TextNode({self.text}, {self.text_type}, {self.url})'
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
